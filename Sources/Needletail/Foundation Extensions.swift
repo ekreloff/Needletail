3c80fb701e2258d4f70b30029c13c3e96d1e9505
Ethan Kreloff <emkreloff@me.com>
2018-08-25 13:01:53 -0600

    Added TimeInterval shortcut syntax

---
+++ Sources/Needletail/Foundation Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Foundation Extensions.swift
+//  Needletail
+//
+//  Created by Ethan Kreloff on 8/18/18.
+//
+
+import Foundation
+
+extension TimeInterval {
+    public var minutes: TimeInterval {
+        return self*60.0
+    }
+    
+    public var hours: TimeInterval {
+        return minutes*60.0
+    }
+    
+    
+    public var days: TimeInterval {
+        return hours*24.0
+    }
+}
