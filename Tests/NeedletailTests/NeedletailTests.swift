aaf6be0f226bdc6838d719e9b1ac27e5274cd473
Ethan Kreloff <emkreloff@me.com>
2019-04-07 22:18:42 -0600

    Beginning to write tests for the framework

--- Tests/NeedletailTests/NeedletailTests.swift
+++ Tests/NeedletailTests/NeedletailTests.swift
@@ -2,14 +2,39 @@ import XCTest
 @testable import Needletail
 
 final class NeedletailTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Needletail().text, "Hello, World!")
+    func testRequest() {
+        let expectaion = XCTestExpectation(description: "Simple Day of Year Test")
+        
+        TimeService.shared.request(Time.self) { (time) in
+            guard let dayOfYear = Calendar.current.ordinality(of: .day, in: .year, for: Date()),
+            let time = time else {
+                XCTFail(); return
+            }
+            
+            XCTAssert(time.day_of_year == dayOfYear + 1)
+            
+            expectaion.fulfill()
+        }
+        
+        wait(for: [expectaion], timeout: 10.0)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testRequest", testRequest),
     ]
+}
+
+
+struct Time: Respondable {
+    static var path: String = "/api/timezone/America/Argentina/Salta"
+    
+    let day_of_year: Int
+}
+
+final class TimeService: Service {
+    static var shared: TimeService = TimeService()
+    
+    private init() {}
+    
+    var baseURL: URL = "http://worldtimeapi.org"
 }
